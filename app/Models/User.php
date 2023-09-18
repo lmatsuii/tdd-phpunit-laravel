ee7ba3277ae9789af75190a2afce8e8bf108e54e
lmatsuii <leonardomatsuii@gmail.com>
2023-09-18 16:12:46 -0300

    feat: Add RegisterController and update User model and routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property-read ind $id
+ * @property string $password
+ * @property string $name
+ * @property string $email
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
